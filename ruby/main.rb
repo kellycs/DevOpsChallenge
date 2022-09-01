94d92e3046064cb59a0b56b46de04de1261fc66d
ShepFC3 <chris.shepherd@flybreeze.com>
2022-09-01 17:53:28 -0600

    Adds auth

--- ruby/main.rb
+++ ruby/main.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 
+$stdout.sync = true
+
 set :port, 8080
 set :bind, "0.0.0.0"
+
+before do
+  halt 401, 'Denied' unless request.env['HTTP_SUPERSECRETACCESSKEY'] == ENV['SUPERSECRETACCESSKEY']
+end
 
 get '/' do
   'It works!'
