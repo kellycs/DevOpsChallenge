11439a6ae88579f5b89fa7c641b82883be9fb46b
bryannxn212 <bryannxn212@gmail.com>
2022-09-02 08:54:01 -0600

    Made cluster more secure ;)

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ module "eks" {
 
   cluster_name    = local.cluster_name
   cluster_version = "1.22"
-  #cluster_secure  = true
+  cluster_secure  = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
